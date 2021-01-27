1768b7e156d223d61e7f37f6ac8968a6889481f0
des-create <78061283+des-create@users.noreply.github.com>
2021-01-27 10:50:53 -0600

    Add files via upload

---
+++ frontend.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+function add(){
+var a,b,c;
+a=Number(document.getElementById("first").value);
+b=Number(document.getElementById("second").value);
+c=a+b;
+document.getElementById("answer").value= c;
+}
+
+
